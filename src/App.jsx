e06e5958f6fae08f2b47995eb0db23aaec480bbb
Di@llo_Codeur <mouhammadsitadiallo@gmail.com>
2025-08-28 23:33:06 +0200

    I'm trying to fix delete feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,59 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { useState } from 'react';
 import './App.css' ;
 import {useFetch} from "./hooks/useFetch.js";
 
+
 function App() {
-  const {loadings, datas, errors} = useFetch("https://jsonplaceholder.typicode.com/todos?_limit=10&_delay=5000");
+  /*const {loadings, datas, errors} = useFetch("https://jsonplaceholder.typicode.com/todos?_limit=10&_delay=5000");*/ 
 
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
-  const [inputedTodo, setInputedTodo] = useState('Read Quran');
+  const [inputedTodos, setInputedTodos] = useState([]);
 
-  const handleAdd = (todo) => {
-    setInputedTodo(todo);
+  const handleAddTodo = () => {
+    if(input.trim() === '') return; 
+    setInputedTodos([input, ...inputedTodos]); //I write it so because with react.js it's not possible to do mutation with arrays and objects
+    setInput("");
   }
 
-  if(loadings) return <p style={{color: "white"}}>Loading...</p>;
-  if(errors) return <p>{errors}</p>;
+  const updateTodos = () => {
+    if(inputedTodos.length === 0) return;
+    setInputedTodos([...inputedTodos, inputedTodos.filter(todo => todo.length > 3)]);
+
+  }
+
+  /*if(loadings) return <p style={{color: "white"}}>Loading...</p>;
+  if(errors) return <p>{errors}</p>;*/ 
 
 return(
   <div className='app-wrapper'> 
     <TodoHead 
     value={input} 
     onInputChange={setInput} 
-    onInputedTodoChange={handleAdd}/>
+    onInputedTodosChange={handleAddTodo}
+    />
     <TodoButtonsFilter/>
     <TodoBody 
-    todos={datas}
-    inputedTodo={inputedTodo}
+    //todos={datas}
+    inputedTodos={inputedTodos}
+    updateTodos={updateTodos}
     />
   </div>
 );
 }
 
-function TodoHead({value, onInputChange, onInputedTodoChange}){
+function TodoHead({value, onInputChange, onInputedTodosChange}){
   return(
     <div className='wrapper-todo-head-childs'>
-       <input type='text' placeholder='Add new todo...' className='input-field' value={value} onChange={e => onInputChange(e.target.value)} />
-       <select className='wrapper-todo-head-childs-select'>
+      <input type='text' placeholder='Add new todo...' className='input-field' value={value} onChange={e => onInputChange(e.target.value)} onKeyDown={e => e.key === 'Enter' && onInputedTodosChange()}/>
+      <select className='wrapper-todo-head-childs-select' defaultValue={"medium"}>
        <option value="urgent">Urgent</option>
-       <option value="medium" selected>Medium</option>
+       <option value="medium">Medium</option>
        <option value="low">Low</option>
        </select>
-       <button type='submit' className='wrapper-todo-head-childs-addbtn' onClick={onInputedTodoChange}>Add</button> 
+      <button type='button' className='wrapper-todo-head-childs-addbtn' onClick={onInputedTodosChange}>Add</button> 
     </div>
   );
 }
@@ -55,13 +68,20 @@ function TodoButtonsFilter(){
     </div>
   );
 }
-function TodoBody({todos, inputedTodo}){
-  console.log(inputedTodo)
+function TodoBody({/*todos*/ inputedTodos, updateTodos}){
+  //console.log(inputedTodos);
+  //const fetchedTodos = todos.map(todo => (todo.title))
+  //let arrayOfTodos = [...inputedTodos, ...fetchedTodos]
+  //console.log(arrayOfTodos.length);
+  //console.log(arrayOfTodos);
   return(
-    <div style={{paddingLeft: "50px", marginTop: "0"}}>
-      <h1>{inputedTodo}</h1>
-      {todos.map(todo => (
-        <h1 key={todo.id}>{todo.title}</h1>
+    <div style={{paddingLeft: "50px", marginTop: "0"}} className="todos-wrapper">
+      {inputedTodos.map(todo => (
+        <h1 key={inputedTodos.indexOf(todo)} className="todo">
+          <input type="checkbox"/>
+          {todo}
+          <button className="delete-button"><FontAwesomeIcon icon={faTrashCan} onClick={updateTodos}/></button>
+        </h1>
       ))}
     </div>
   );
