55f3623f854d6e18d9e355510d0c7a34cb4d412b
Di@llo_Codeur <mouhammadsitadiallo@gmail.com>
2025-09-07 17:00:57 +0200

    Priorities feature is now fixed, there is no bug

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,19 @@ function App() {
 
   const [priority, setPriority] = useState('');
 
+  const [allTodos, setAllTodos] = useState([]);
+
   const handleAddTodo = () => {
     if(input.trim() === '') return; 
-    setInputedTodos([{id: Date.now(), text: input, priority: priority}, ...inputedTodos]); //I write it so because with react.js it's not possible to mutate arrays and objects
+    setInputedTodos([{id: Date.now(), text: input, priority: priority ? priority : "medium"}, ...inputedTodos]); //I write it so because with react.js it's not possible to mutate arrays and objects
+    setAllTodos([{id: Date.now(), text: input, priority: priority ? priority : "medium"}, ...allTodos])
     setInput(""); 
   }
 
   const updateTodos = (id) => {
     if(inputedTodos.length === 0) return;
     setInputedTodos(inputedTodos.filter(todo => todo.id !== id));// Here it's not necessary to use the spread operator because filter returns a new array (a shallow copy) and does not mutate inputedTodos.
+    setAllTodos(allTodos.filter(todo => todo.id !== id));
   }
 
   const handleFinishedbtnOpacity = (id) => {
@@ -32,6 +36,15 @@ function App() {
     }))
   }
   const isAllCheckboxesUnchecked = Object.values(checkedItems).every((item) => item === false);
+
+  const displayTodosDependingOnPriority = (priority) => {
+    if(allTodos.length === 0) return;
+    if(priority === null){
+      return setInputedTodos(allTodos);
+    }
+    console.log(allTodos);
+    setInputedTodos(allTodos.filter(todo => todo.priority.toLowerCase() === priority.toLowerCase()));
+  }
 
 return(
   <div className='app-wrapper'> 
@@ -44,6 +57,7 @@ return(
     />
     <TodoButtonsFilter
     isAllCheckboxesUnchecked={isAllCheckboxesUnchecked}
+    displayTodosDependingOnPriority={displayTodosDependingOnPriority}
     />
     <TodoBody 
     inputedTodos={inputedTodos}
@@ -60,7 +74,7 @@ function TodoHead({value, onInputChange, onInputedTodosChange, priority, onPrior
   return(
     <div className='wrapper-todo-head-childs'>
       <input type='text' placeholder='Add new todo...' className='input-field' value={value} onChange={e => onInputChange(e.target.value)} onKeyDown={e => e.key === 'Enter' && onInputedTodosChange()}/>
-      <select className='wrapper-todo-head-childs-select' value={priority} onChange={e => onPriorityChange(e.target.value)}>
+      <select className='wrapper-todo-head-childs-select' value={priority ? priority : "medium"} onChange={e => onPriorityChange(e.target.value)}>
        <option value="medium">Medium</option>
        <option value="urgent">Urgent</option>
        <option value="low">Low</option>
@@ -69,13 +83,13 @@ function TodoHead({value, onInputChange, onInputedTodosChange, priority, onPrior
     </div>
   );
 }
-function TodoButtonsFilter({isAllCheckboxesUnchecked}){
+function TodoButtonsFilter({isAllCheckboxesUnchecked, displayTodosDependingOnPriority}){
   return(
     <div className='wrapper-todo-buttons-filter-childs'>
-      <button type='button' className='wrapper-todo-buttons-filter-childs-all-allbtn'>All</button>
-      <button type='button' className='wrapper-todo-buttons-filter-childs-urgentbtn'>Urgent</button>
-      <button type='button' className='wrapper-todo-buttons-filter-childs-mediumbtn'>Medium</button>
-      <button type='button' className='wrapper-todo-buttons-filter-childs-lowbtn'>Low</button>
+      <button type='button' className='wrapper-todo-buttons-filter-childs-all-allbtn' onClick={() => displayTodosDependingOnPriority(null)}>All</button>
+      <button type='button' className='wrapper-todo-buttons-filter-childs-urgentbtn' onClick={() => displayTodosDependingOnPriority('urgent')}>Urgent</button>
+      <button type='button' className='wrapper-todo-buttons-filter-childs-mediumbtn' onClick={() => displayTodosDependingOnPriority('medium')}>Medium</button>
+      <button type='button' className='wrapper-todo-buttons-filter-childs-lowbtn' onClick={() => displayTodosDependingOnPriority('low')}>Low</button>
       <button className='wrapper-todo-buttons-filter-childs-finishbtn'style={{opacity: isAllCheckboxesUnchecked ? 0.2 : 1, ...(!isAllCheckboxesUnchecked && {backgroundColor: "rgb(52, 175, 175)"})}}>Finish selectionned todos</button>
     </div>
   );
@@ -86,7 +100,7 @@ function TodoBody({inputedTodos, updateTodos, onOpacityChange, checkedItems}){
       {inputedTodos.map((todo, index) => ( 
         <h1 key={todo.id} className="todo">
           <input name={todo} key={index} type="checkbox" checked={!!checkedItems[todo.id]} onChange={() => onOpacityChange(todo.id)}/>
-          {todo.text}  <span className={todo.priority ? todo.priority.toLowerCase() : "medium"}>{todo.priority.toLowerCase() ? todo.priority : "medium"}</span>
+          {todo.text}  <span className={todo.priority ? todo.priority.toLowerCase() : "medium"}>{todo.priority ? todo.priority : "medium"}</span>
           <button className="delete-button" onClick={() => updateTodos(todo.id)}><FontAwesomeIcon icon={faTrashCan}/></button>
         </h1>
       ))}
